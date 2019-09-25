0ec7a226b925e87bfa3b658700a52ea3bbf4df08
Julian Klaiber <julian@klaiber.me>
2019-09-25 15:58:44 +0200

    Aufräumen...

--- configuration.py
+++ configuration.py
@@ -30,15 +30,6 @@ def get_hostname(host: dict) -> str:
         password=host['password'])
     return response
 
-def set_hostname(host: dict) -> str:
-    test_string = '<hostname xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native"  xmlns:ios="http://cisco.com/ns/yang/Cisco-IOS-XE-native">test</hostname>'
-    response = restconf_helpers.RestconfRequestHelper().patch(
-        url=f'https://{host["connection_address"]}/restconf/data/Cisco-IOS-XE-native:native/hostname/',
-        username=host['username'],
-        password=host['password'],
-        data=test_string)
-    return response
-
 def patch_configuration(host: dict) -> str:
     rendering_data = rendering.RenderJinjaTemplate().rendering(host)
     #rendering_data = rendering_data.replace('\n', '')
@@ -68,14 +59,4 @@ if __name__ == '__main__':
     init_logger()
     main()
 
-'''
-def print_interfaces(host: dict) -> None:
-    print(get_interfaces(host=host))
 
-def get_interfaces(host: dict) -> str:
-    response = restconf_helpers.RestconfRequestHelper().get(
-        url=f'https://{host["connection_address"]}/restconf/data/Cisco-IOS-XE-native:native/interface/',
-        username=host['username'],
-        password=host['password'])
-    return response
-'''
